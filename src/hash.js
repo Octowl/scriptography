16e3fb94382f010eca38106f1fa75801e33ef910
Octowl <alsaff1987@gmail.com>
2016-05-21 11:54:44 -0400

    Implement random number generator

--- src/hash.js
+++ src/hash.js
@@ -7,7 +7,13 @@ var hash = {};
 
 hash.simple = {};
 
-hash.simple._pad = function () {};
+hash.simple._pad = function (str, length) {
+    var newString;
+    for (var i = 0; i < length; i++) {
+        newString += str[i % str.length]
+    }
+    return newString
+};
 
 hash.simple._partition = function () {};
 
