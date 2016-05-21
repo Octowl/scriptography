38cc1f0e784a7d26ae829dbdbeff24c686bf5b45
Octowl <alsaff1987@gmail.com>
2016-05-21 11:50:28 -0400

    Implement random number generator

--- src/random.js
+++ src/random.js
@@ -4,9 +4,24 @@ var base64 = require('./base64');
 
 var random = {};
 
-random.integer = function () {};
+random.integer = function (min, max) {
+    var args = [].slice.call(arguments);
+    if (args.length < 2) {
+        max = min;
+        min = 0;
+    }
+    return Math.round(Math.random() * max) + min;
+};
 
 var charSet = base64._charSet;
-random.base64 = function () {};
+random.base64 = function (length) {
+    var randStr = [];
+
+    for (var i = 0; i < length; i++) {
+        randStr.push(random.integer(64));
+    }
+
+    return base64.fromDigits(randStr);
+};
 
 module.exports = random;
